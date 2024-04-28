27c39a2d18ef312018d38501402b930a12fe67e8
Stoyan <podlobg@gmail.com>
2024-04-28 17:16:45 +0300

    up

--- 11.Set DEFAULT CURRENT TIME- GETDATE().sql
+++
@@ -1,9 +0,0 @@
-
-ALTER TABLE [Users]
-ADD CONSTRAINT DE_CurrentTime DEFAULT GETDATE() FOR [LastLoginTime]
-
-INSERT INTO [Users] ([UserName], [Password], [IsDeleted])
-		VALUES		
-	('CurrTime', 'ssgg34','false')
-
-SELECT * FROM [Users]
--- Set CONSTRAINT For password length.sql
+++
@@ -1,8 +0,0 @@
--- Set constraint for password length
-ALTER TABLE [Users]
-	ADD CONSTRAINT CHK_PasswordLength CHECK (LEN([Password]) >= 5)
-
-
-INSERT INTO [Users] ([UserName], [Password], [LastLoginTime], [IsDeleted])
-		VALUES	
-('OOOOas', 'oos', '18:40:34', 'false')
