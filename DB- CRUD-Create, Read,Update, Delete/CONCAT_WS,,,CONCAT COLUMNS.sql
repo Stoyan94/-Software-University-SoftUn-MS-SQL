b532dabc00ee88f23bb100e5ac6a011015ee6a38
Stoyan <podlobg@gmail.com>
2024-05-01 14:22:22 +0300

    UP

---
+++ DB- CRUD-Create, Read,Update, Delete/CONCAT_WS,,,CONCAT COLUMNS.sql
@@ -0,0 +1,15 @@
+
+--This line introduces the CONCAT_WS function, which concatenates its arguments into a single string, separated by a specified separator.
+
+--In this case, the function concatenates the FirstName, MiddleName, and LastName columns from the Employees table, separated by a space (' '), to create a full name.
+
+--The AS FullName part aliases the result of this concatenation as FullName, making it more readable in the result set.
+
+SELECT CONCAT_WS(' ',FirstName, MiddleName, LastName)
+FirstName, 
+LastName, 
+Salary, 
+d.[Name]
+FROM Employees AS e JOIN Departments d ON e.DepartmentID = d.DepartmentID
+WHERE e.DepartmentID = 1
+ORDER BY Salary DESC
