d5f30258b03c341033da5987e31ca681577ac2ee
Stoyan <podlobg@gmail.com>
2024-05-02 15:44:40 +0300

    up

---
+++ UPDATE.sql
@@ -0,0 +1,33 @@
+
+UPDATE [Employees]
+SET [LastName] = 'Brown'
+WHERE EmployeeID = 1
+
+
+-- UPDATE [Employees]: This part of the statement specifies the table that will be updated. 
+	--In this case, it's the [Employees] table.
+
+--SET Salary = Salary * 1.10: This part of the statement specifies the columns and the values they will be updated to. 
+	--It increases the Salary column by 10% for the records that meet the condition.
+
+--JobTitle = 'Senior' + JobTitle: This part of the statement appends the string 'Senior' to the beginning of the values in the JobTitle column. For example, 
+	--if the JobTitle was 'Engineer', it would become 'SeniorEngineer'.
+
+--In summary, this UPDATE statement increases the Salary by 10% and adds the prefix 'Senior' to the JobTitle for employees in department 3 in the [Employees] table.
+
+UPDATE [Employees]
+SET Salary = Salary *1.10,
+	JobTitle = 'Senior ' + JobTitle
+WHERE DepartmentID = 3
+
+
+SELECT [Salary], [JobTitle]
+FROM [Employees]
+WHERE DepartmentID = 3
+
+UPDATE [Projects]
+SET [EndDate] = GETDATE()
+WHERE [EndDate] IS NULL
+
+SELECT [EndDate]
+FROM  [Projects]
