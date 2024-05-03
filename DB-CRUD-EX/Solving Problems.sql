9a4f2b4f5632edabe298aa1fb66c60aebc8e35a7
Stoyan <podlobg@gmail.com>
2024-05-03 17:26:40 +0300

    UP

--- DB-CRUD-EX/Solving Problems.sql
+++ DB-CRUD-EX/Solving Problems.sql
@@ -1,6 +1,33 @@
 -- 2.Find All the Information About Departments
-SELECT * FROM [Departments]
+SELECT * 
+FROM [Departments]
 
 
- --3.	Find all Department Names
- SELECT [Name] FROM [Departments]
+
+ --3.Find all Department Names
+SELECT [Name] 
+FROM [Departments]
+
+
+
+ -- 4.Find Salary of Each Employee
+SELECT 
+	[FirstName] 
+	,[LastName] 
+	,[Salary] 
+FROM [Employees]
+
+
+
+--5.Find Full Name of Each Employee
+SELECT 
+	[FirstName]
+	,[MiddleName]
+	,[LastName]
+FROM [Employees]
+
+
+--6.Find Email Address of Each Employee
+SELECT CONCAT_WS('', [FirstName], '.', [LastName], '@', 'softuni.bg')
+AS [Full Email Address]
+FROM [Employees]
