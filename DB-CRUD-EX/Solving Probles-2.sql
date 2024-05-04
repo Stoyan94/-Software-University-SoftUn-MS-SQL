87780b970c20dd8bd68f15f9be2b2289dc68eebc
Stoyan <podlobg@gmail.com>
2024-05-04 17:08:36 +0300

    up

--- DB-CRUD-EX/Solving Probles-2.sql
+++ DB-CRUD-EX/Solving Probles-2.sql
@@ -10,8 +10,28 @@ FROM [Employees]
 --19.Find First 10 Started Projects
 --Create a SQL query that finds the first 10 projects which were started, 
 --select all the information about them and order the result by starting date, then by name.
-SELECT TOP 10 *
+SELECT 
+TOP 10 *
 FROM [Projects]
 ORDER BY 
 		[StartDate]
 	   ,[Name]
+
+
+
+
+--20.	Last 7 Hired Employees
+--Create a SQL query that finds the last 7 hired employees, select their first, last name and hire date. 
+--Order the result by hire date descending.
+SELECT 
+TOP 7 
+   [FirstName]
+   ,[LastName]
+   ,[HireDate]
+FROM [Employees]
+ORDER BY [HireDate] DESC
+
+
+
+
+
