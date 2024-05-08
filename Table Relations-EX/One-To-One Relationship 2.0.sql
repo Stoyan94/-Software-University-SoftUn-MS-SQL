0bf9cd62e0c217064d3501a00de51108a205570c
Stoyan <podlobg@gmail.com>
2024-05-08 15:00:14 +0300

    UP

---
+++ Table Relations-EX/One-To-One Relationship 2.0.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [Passports](
+	[PassportID] INT PRIMARY KEY IDENTITY(101,1)
+   ,[PassportNumber] VARCHAR(8) NOT NULL
+)
+
+CREATE TABLE [Persons](
+	[PersonID] INT PRIMARY KEY IDENTITY(1,1)
+   ,[FirstName] VARCHAR(50) NOT NULL
+   ,[Salary] DECIMAL (8,2) NOT NULL
+   ,[PassportID] INT FOREIGN KEY REFERENCES [Passports](PassportID) UNIQUE NOT NULL
+)
+
+
+INSERT INTO [Passports]
+	VALUES
+	('N34FG21B'),
+	('K65LO4R7'),
+	('ZE657QP2')
+
+
