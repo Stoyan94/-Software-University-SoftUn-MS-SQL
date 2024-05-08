6976941b28b3bed8499f2a6a160eeb0dea716c96
Stoyan <podlobg@gmail.com>
2024-05-08 16:30:32 +0300

    up

--- Table Relations-EX/2.One-To-Many Relationship.sql
+++ Table Relations-EX/2.One-To-Many Relationship.sql
@@ -9,3 +9,20 @@ CREATE TABLE [Models](
    ,[Name] VARCHAR(50) NOT NULL
    ,[ManufacturerID] INT FOREIGN KEY REFERENCES [Manufacturers]([ManufacturerID]) NOT NULL
 )
+
+
+INSERT INTO [Manufacturers]
+	VALUES
+	('BWM', '07/03/1916')
+   ,('Tesla', '01/01/2003')
+   ,('Lada', '01/05/1966')
+
+
+INSERT INTO [Models]
+	VALUES
+	('X1', 1)
+   ,('i6', 1)
+   ,('Model S', 2)
+   ,('Model X', 2)
+   ,('Model 3', 2)
+   ,('Nova', 3)
