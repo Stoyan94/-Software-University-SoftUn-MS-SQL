4cc63403c494377a947849260871dde10e8fee70
Stoyan <podlobg@gmail.com>
2024-05-09 13:59:28 +0300

    up

---
+++ Table Relations-EX/5.Online Store Database.sql
@@ -0,0 +1,36 @@
+
+-- Items info
+CREATE TABLE [ItemTypes](
+	[ItemTypeID] INT PRIMARY KEY IDENTITY
+   ,[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Items](
+	[ItemID] INT PRIMARY KEY IDENTITY 
+   ,[Name] VARCHAR(50) NOT NULL
+   ,[ItemTypeID] INT FOREIGN KEY REFERENCES [ItemTypes]([ItemTypeID]) NOT NULL
+)
+
+-- Customers info
+CREATE TABLE [Cities](
+	[CityID] INT PRIMARY KEY IDENTITY
+   ,[Name] VARCHAR(50) NOT NULL
+)
+
+CREATE TABLE [Customers](
+	[CustomerID] INT PRIMARY KEY IDENTITY
+   ,[Name] VARCHAR(50) NOT NULL
+   ,[Birthday] DATE NOT NULL
+   ,[CityID] INT FOREIGN KEY REFERENCES [Cities]([CityID]) NOT NULL
+)
+
+-- Orders info
+CREATE TABLE [Orders](
+	[OrderID] INT PRIMARY KEY IDENTITY
+   ,[CustomerID] INT FOREIGN KEY REFERENCES [Customers]([CustomerID]) NOT NULL
+)
+
+CREATE TABLE [OrderItems](
+	[OrderID] INT FOREIGN KEY REFERENCES [Orders]([OrderID]) NOT NULL
+   ,[ItemID] INT FOREIGN KEY REFERENCES [Items]([ItemID]) NOT NULL
+)
