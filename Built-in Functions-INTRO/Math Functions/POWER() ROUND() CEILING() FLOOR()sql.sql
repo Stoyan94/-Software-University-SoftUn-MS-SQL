3d631810891aec2a4715720febbc6b21ad586797
Stoyan <podlobg@gmail.com>
2024-05-12 15:03:10 +0300

    up

---
+++ Built-in Functions-INTRO/Math Functions/POWER() ROUND() CEILING() FLOOR()sql.sql
@@ -0,0 +1,19 @@
+-- Example: Calculate 2 raised to the power of 3
+SELECT POWER(2, 3) AS Result;
+-- Output: Result
+--         8
+
+-- Example: Round 3.14159 to two decimal places
+SELECT ROUND(3.14159, 2) AS Result;
+-- Output: Result
+--         3.14
+
+-- Example: Find the smallest integer greater than or equal to 4.2
+SELECT CEILING(4.2) AS Result;
+-- Output: Result
+--         5
+
+-- Example: Find the largest integer less than or equal to 4.9
+SELECT FLOOR(4.9) AS Result;
+-- Output: Result
+--         4
