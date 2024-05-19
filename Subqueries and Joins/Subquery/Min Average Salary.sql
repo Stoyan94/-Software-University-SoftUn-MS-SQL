8fb6619a4f8e44714b7ee5761356488067e3b7bd
Stoyan <podlobg@gmail.com>
2024-05-19 15:28:10 +0300

    UP

---
+++ Subqueries and Joins/Subquery/Min Average Salary.sql
@@ -0,0 +1,12 @@
+SELECT 
+MIN(avs.MinAVG) AS MinAverageSalary 
+FROM
+(
+	SELECT AVG(Salary) AS MinAVG
+	FROM Employees
+	GROUP BY DepartmentID
+) AS avs
+
+
+--When we use GROUP BY, we can use in SELECT only the column we use in GROUP BY, but if we want to use other columns in SELECT 
+	-- ONLY WITH THE AGGREGATE FUNCTION, we can use other columns in SELECT
