8fe253c21633709dc6761c589279774d4ed5b71f
Stoyan <podlobg@gmail.com>
2024-05-23 19:45:11 +0300

    UP

--- Subqueries-and-JOINs-Exercises/GROUP BY + AGG.FUNCS EX.sql
+++ Subqueries-and-JOINs-Exercises/GROUP BY + AGG.FUNCS EX.sql
@@ -4,3 +4,11 @@ SELECT [JobTitle]
 FROM Employees
 WHERE [JobTitle] = 'Accountant'
 GROUP BY [JobTitle]
+
+
+SELECT [JobTitle]
+	   ,AVG(Salary) AS [Average Salary]
+	   ,COUNT(EmployeeID) AS [Workers Count]
+FROM Employees
+GROUP BY [JobTitle]
+HAVING AVG(Salary) > 25000
