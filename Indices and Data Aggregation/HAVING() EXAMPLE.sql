2ea12cb2ec8213e73fe0066a6d98dece388758fe
Stoyan <podlobg@gmail.com>
2024-05-25 14:59:47 +0300

    up

--- Indices and Data Aggregation/HAVING() EXAMPLE.sql
+++ Indices and Data Aggregation/HAVING() EXAMPLE.sql
@@ -1,7 +1,15 @@
-SELECT 
+﻿SELECT 
 	DepartmentID
 	,SUM(Salary) AS TotalSalary
 FROM Employees
 GROUP BY DepartmentID
 HAVING SUM(Salary) >=150000
 ORDER BY DepartmentID
+
+-- The HAVING clause is used to filter data based on
+-- aggregate values
+-- ▪ We cannot use it without grouping first
+-- ▪ Aggregate functions (MIN, MAX, SUM etc.) are
+-- executed only once
+-- ▪ Unlike HAVING, WHERE filters rows before
+-- aggregation
