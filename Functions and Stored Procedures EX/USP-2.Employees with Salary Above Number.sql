6efb0bd704c42827868f69972b81c74baee30b86
Stoyan <podlobg@gmail.com>
2024-06-03 19:54:25 +0300

    up

---
+++ Functions and Stored Procedures EX/USP-2.Employees with Salary Above Number.sql
@@ -0,0 +1,11 @@
+CREATE PROC usp_GetEmployeesSalaryAboveNumber @minSalary DECIMAL(18,4)
+AS 
+	BEGIN
+		SELECT FirstName
+			  ,LastName
+		FROM Employees
+		WHERE Salary >= @minSalary
+	END
+
+
+EXEC usp_GetEmployeesSalaryAboveNumber 48100
