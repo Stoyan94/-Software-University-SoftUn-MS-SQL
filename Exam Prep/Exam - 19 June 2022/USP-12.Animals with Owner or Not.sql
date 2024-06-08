791e77c9132ed50d883b57d204a50721c08c322f
Stoyan <podlobg@gmail.com>
2024-06-08 17:31:32 +0300

    UP

---
+++ Exam Prep/Exam - 19 June 2022/USP-12.Animals with Owner or Not.sql
@@ -0,0 +1,12 @@
+CREATE PROC usp_AnimalsWithOwnersOrNot(@AnimalName VARCHAR(30))
+AS
+	BEGIN
+		 SELECT a.[Name]
+				,ISNULL(o.[Name], 'For adoption')
+		 FROM Animals AS a
+		 LEFT JOIN Owners AS o ON a.OwnerId = o.Id
+		 WHERE a.[Name] = @AnimalName
+	END
+
+
+	EXEC usp_AnimalsWithOwnersOrNot 'Pumpkinseed Sunfish'
