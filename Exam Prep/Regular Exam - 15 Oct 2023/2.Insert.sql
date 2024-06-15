1f195e4a001edef65a73fc649459f463a29ac28f
Stoyan <podlobg@gmail.com>
2024-06-15 14:06:56 +0300

    up

---
+++ Exam Prep/Regular Exam - 15 Oct 2023/2.Insert.sql
@@ -0,0 +1,7 @@
+﻿INSERT INTO Tourists (Name, PhoneNumber, Email, CountryId)
+VALUES 
+    ('John Rivers', '653-551-1555', 'john.rivers@example.com', 6),
+    ('Adeline Aglaé', '122-654-8726', 'adeline.aglae@example.com', 2),
+    ('Sergio Ramirez', '233-465-2876', 's.ramirez@example.com', 3),
+    ('Johan Müller', '322-876-9826', 'j.muller@example.com', 7),
+    ('Eden Smith', '551-874-2234', 'eden.smith@example.com', 6);
